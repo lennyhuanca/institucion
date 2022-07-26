45ec0acf5deea23681fb099824eaf30cb7d57f4b
lennyhuanca <lennyhuancachurqui@outlook.com>
2022-07-26 16:40:49 -0400

    a

--- cursos/index.php
+++ cursos/index.php
@@ -14,4 +14,35 @@ $materias=$conexion->seleccionar_tabla('materia');
 
 var_dump($alumnos, $materias);
 
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTRO</title>
+</head>
+<body>
+    <center>
+        <h3>REGISTRO</h3>
+        <table>
+            <tr>
+                <td>Codigo</td>
+                <td>Nombre</td>
+                <td>Apellido paterno</td>
+                <td>Apellido materno</td>
+                <td>C.i.</td>
+                <td>Telefono</td>
+            </tr> 
+            <?php
+
+foreach ($alumnos as $dato) {
+}
+?>  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+        </table>
+    </center>
+</body>
+</html>
